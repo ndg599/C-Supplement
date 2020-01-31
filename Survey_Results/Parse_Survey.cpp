ce42aba99fc0bc80c1e599fca49e6e3242da4b1f
Landen <Lmarchan@kent.edu>
2020-01-31 13:16:20 -0500

    Survey Results

--- Survey_Results/Parse_Survey.cpp
+++ Survey_Results/Parse_Survey.cpp
@@ -13,21 +13,21 @@ int main()
 	int           q, commentNum = -1, totalTaken = 0, totals[15][3] {0};
 
 	std::string questions[] = { "How often do you use a C++ supplement (1 = often, 2 = at times, 3 = never)?",
-		                        "Would you like a C++ supplement tailored to CSI - CSIII?",
-								"Would you be interested in learning about C++ topics not discussed?",
-								"Would you be interested in having a deeper understanding of C++ topics discussed",
-								"Best device (1 = Desktop, 2 = Tablet, 3 = Phone)?",
-								"Color-coordination helps?",
-								"1 = Monospaced vs 2 = Proportional?",
-								"1 = Dark vs 2 = Light?",
-								"Fill-in / multiple choice?",
-								"Comment section?",
-								"Instant chat?",
-								"Been told that using namespace std; is not recommended?",
-								"Output of f = 0.2",
-								"Output of f = 0.25",
-								"Output of d = 0.0/0.0",
-								"Additional comments: "
+                                "Would you like a C++ supplement tailored to CSI - CSIII?",
+                                "Would you be interested in learning about C++ topics not discussed?",
+                                "Would you be interested in having a deeper understanding of C++ topics discussed",
+                                "Best device (1 = Desktop, 2 = Tablet, 3 = Phone)?",
+                                "Color-coordination helps?",
+                                "1 = Monospaced vs 2 = Proportional?",
+                                "1 = Dark vs 2 = Light?",
+                                "Fill-in / multiple choice?",
+                                "Comment section?",
+                                "Instant chat?",
+                                "Been told that using namespace std; is not recommended?",
+                                "Output of f = 0.2",
+                                "Output of f = 0.25",
+                                "Output of d = 0.0/0.0",
+                                "Additional comments: "
 	                          };
 	std::vector<std::string> comments;
 	
@@ -58,8 +58,7 @@ int main()
 			output << questions[num] << ":\n";
 			for (q = 0, q = 0; q < 3; ++q) 
 				output << "\tQ" << num+1 << " - " << q+1 << ": " << totals[num][q]
-					      << "  Percentage: " << static_cast<double>(totals[num][q]) / totalTaken 
-						  << "%\n";
+					      << "  Percentage: " << static_cast<double>(totals[num][q]) / totalTaken << "%\n";
 			std::endl(output);
 		}
 		for (auto e : comments)
