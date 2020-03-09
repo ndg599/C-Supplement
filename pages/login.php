a95fd627346e060897219ba9127592ac9fde62a4
ndg599 <ndg599@gmail.com>
2020-03-08 21:01:14 -0400

    Comments and include on login.php

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../pdoconfig.php');
+
 // Setup link to database
 $conn = mysqli_connect($servername, $username, $password, $database);
 
@@ -7,15 +8,17 @@ if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 
+// Query the username and password given in index.html
 $query = "SELECT Username FROM Login WHERE Username=\"" .
-$_POST["username"] . "\" && password=\"" . $_POST["password"] . "\"";
-#echo $query; echo "\n";
+	$_POST["username"] . "\" && password=\"" . $_POST["password"] . "\"";
 $result = mysqli_query($conn, $query);
 
+// Should only have one match for unique username and password
 if (mysqli_num_rows($result) == 1) {
-	echo "Login successful";
+	echo "Login successfull"; // Temporary, for testing
+	include 'login_success.php'; // Handles successful login
 }
 else {
-	echo "Login failed";
+	echo "Login failed"; // Temporary, should appear live on login.html
 }
 ?>
