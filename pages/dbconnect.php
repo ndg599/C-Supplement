a104ed851ff985c25e5c8400417ff40c2a29525d
bjacob12 <56331778+bjacob12@users.noreply.github.com>
2020-03-09 01:37:13 -0400

    Add files via upload

--- pages/dbconnect.php
+++ pages/dbconnect.php
@@ -1,13 +1,1 @@
-<?--Used to open database-->
-<?php
-require_once('../pdoconfig.php');
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-         
- if(! $conn ){
-               die('Could not connect: ' . mysqli_error($conn));
-  }
-  else
-	  echo "Database Connection was successful";
-?>
+//
