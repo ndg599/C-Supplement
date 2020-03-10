b15c9a4cbe90020242bd5dd4318a7b49f32de6b1
Landen <Lmarchan@kent.edu>
2020-03-10 15:52:07 -0400

    Added article.php prototype

---
+++ pages/article.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Kent C++ Supplement</title>
+	<meta charset="utf-8">
+	<meta name="description" content="C++ Supplement for Kent State - Stark">
+	<meta name="keywords" content="meta description, c++, Kent, State, Stark">
+	<!-- Required as per Bootstrap documentation to utilize responsiveness -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap css file / Our custom css file (index_CSS) -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="./css/index_CSS.css">
+	<!-- Fontawesome css file (for 'emoticons' like the search magnifying glass) / Google custom fonts -->
+	<link href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" rel="stylesheet"/> 
+	<link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet"> <!-- VT323 (experimental font) -->
+	<link href="https://fonts.googleapis.com/css?family=Courier+Prime&display=swap" rel="stylesheet"> <!-- Courier Prime (monospace) IN USE -->
+
+</head>
+<body>
+
+	<div class="container-fluid border rounded-0 banner-sm mt-5">
+		<span>Please keep browser &gt; 575px</span>
+	</div>
+	
+	<nav class="navbar navbar-dark navbar-expand-834">
+		<a class="navbar-brand" href="#"><img src="./img/K++_2.png" alt="Kent C++ Logo"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"
+		        aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle naviation"       >
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav nav nav-pills mr-auto mt-2 mt-md-0">	
+				<li class="nav-item">
+					<a class="nav-link mr-2 csI" href="#">CS I<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link mr-2 csII" href="#">CS II</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link csIII mr-2" href="#">CS III</a>
+				</li> 
+				<li class="nav-item">
+					<a class="nav-link csI" href="#">Chat</a>
+				</li>  	  
+			</ul>
+			<form class="form-inline  ml-auto mr-2">
+				<input class="form-control " type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-primary my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+			</form>
+			<form action="https://www.kentcpp.com/pages/login.html">
+				<button class="btn btn-outline-primary mr-2 btn_mgn" type="submit">Login</button>
+			</form>
+		</div>  
+	</nav>
+
+	<div class="container-fluid rounded-0 banner">
+		<span>Kent State's C++ Code Course</span>
+	</div>
+
+	<div class="content">
+		<article>
+			<div class="container-fluid mt-3">
+				<h1 class="text-white intro">Article Title</h1>
+				<hr>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 section">
+						<p>Overview of topic if there are subtopics</p>
+						<br>
+						<p>If there are no subtopics, this is the where the lesson goes. Colors and design may change
+						 as this is a prototype<p>
+						<br>
+						<p>
+						<figure>
+							<img src="./img/insert.png" alt="for loop flow" class="mb-5 ml-2">
+							<figcaption>Example figure caption</figcaption>
+						</figure>
+						Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text 
+						Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text 
+						Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text 
+						</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="container-fluid mt-5">
+				<h3 class="text-white intro">Subtopic</h3>
+				<hr>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 section mb-5">
+						<p>Subtopic, a focus on one aspect of the overall article</p>
+						<br>
+						<p>
+						<figure>
+							<img src="./img/forLoop3.png" alt="for loop flow" class="mb-5 ml-2">
+							<figcaption>Example visual for a FOR LOOP</figcaption>
+						</figure>
+						An example of a subtopic would be the FOR LOOP topic. Another subtopic would be the DO WHILE LOOP 
+						as well as the WHILE loop, There would be three subtopics for the Looping article.
+						</p>
+						<p>Here is example code:</p>
+						<div class="row">
+							<div class="col-9 Code_Ex">
+							<pre><span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:magenta">#include &lt;iostream&gt;</code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:magenta">#include &lt;string&gt;</code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code></code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:green"><span style="color:cyan">int</span> main()</code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:green">{</code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:green"><span style="color:cyan">    int</span> x = <span style="color:magenta">100</span>;</code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:green">    <span style="color:red">return</span> <span style="color:magenta">0</span>;</code></span>
+									<span class="Table_Row"><span class="Line_Num" style="color:gray"></span><code style="color:green">}</code></span></pre>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="container-fluid mt-5">
+				<h3 class="text-white intro">Video</h3>
+				<hr>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 text-white">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/Rub-JsjMhWY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>	
+					</div>
+				</div>
+			</div>
+			
+			<div class="container-fluid mt-5">
+				<h3 class="text-white intro">Quiz</h3>
+				<hr>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 text-white">
+						<form name="FORM" action="../databaseconnect.php" method="post"> 
+							<label class="mt-4 mb-4">1) How often do you (will you) use a C++ supplement for your Computer Science I - III courses in general
+									(i.e. websites, such as stackoverflow.com and cplusplus.com)?</label>
+								<br><br>
+								<label class="qBox">(Very) often
+									<input type="radio" name="Q1_selection" value="1" required>
+									<span class="checkmark"></span>
+								</label>
+								<label class="qBox">At times
+									<input type="radio" name="Q1_selection" value="2">
+									<span class="checkmark"></span>
+								</label>					
+								<label class="qBox">Never
+									<input type="radio" name="Q1_selection" value="3">
+									<span class="checkmark"></span>
+								</label>
+								
+							<label class="mt-4 mb-4">2) Would you like a C++ supplement tailored to the class itself versus having to sift through articles that
+									may contain information not yet understood (e.g. while in CSI, you look up arrays as function 
+									arguments and come across the statement "an array decays to a pointer when passed to a function")?</label>
+								<br><br>
+								<label class="qBox">Yes
+									<input type="radio" name="Q2_selection" value="1" required>
+									<span class="checkmark"></span>
+								</label>
+								<label class="qBox">No
+									<input type="radio" name="Q2_selection" value="2">
+									<span class="checkmark"></span>
+								</label>		
+
+							<label class="mt-4 mb-4">3) Would you be interested in learning about C++ topics not discussed in class?</label>
+								<br><br>
+								<label class="qBox">Yes
+									<input type="radio" name="Q3_selection" value="1" required>
+									<span class="checkmark"></span>
+								</label>
+								<label class="qBox">No
+									<input type="radio" name="Q3_selection" value="2">
+									<span class="checkmark"></span>
+								</label>				
+							
+							<label class="mt-4">15) What is the output of the following code snippet (assume using std::cout;)?</label>
+							<div class="code mb-4">
+										&emsp;&emsp;double d = 0.0/0.0;
+									<br>
+										&emsp;&emsp;if (d == d)
+									<br>
+										&emsp;&emsp;&emsp;cout &lt;&lt; <q>EQ</q>;
+									<br>
+										&emsp;&emsp;else
+									<br>
+										&emsp;&emsp;&emsp;cout &lt;&lt; <q>NOT_EQ</q>;
+							</div>
+								<label class="qBox">EQ
+									<input type="radio" name="Q15_selection" value="1" required>
+									<span class="checkmark"></span>
+								</label>
+								<label class="qBox">NOT_EQ
+									<input type="radio" name="Q15_selection" value="2">
+									<span class="checkmark"></span>
+								</label>
+								<label class="qBox">Compilation error: Floating Point Error (SIGFPE)
+									<input type="radio" name="Q15_selection" value="2">
+									<span class="checkmark"></span>
+								</label>
+							<br><br>
+						</form>
+					</div>
+				</div>
+			</div>
+			
+			<div class="container-fluid mt-5">
+				<h3 class="text-white intro">Comments</h3>
+				<hr>
+			</div>
+			<div class="container">
+				<div class="row mb-5">
+					<div class="col-md-8 col-12">
+						<form action="" method="post">
+							<div class="input-group">
+								<textarea type="text" rows="10" name="comment" value="comment" placeholder="Enter Comment..." class="form-control"></textarea>
+							</div>
+							<button type="submit" class="btn btn-primary mt-2">Submit</button>
+						</form>
+					</div>
+				</div>
+				<div class="row mb-5">
+					<div class="col-12 section">
+						<!-- PHP Function would be good here, maybe -->
+						<p>User Comment</p>
+						<i class="fa fa-reply"> Reply</i>
+						<span> | </span>
+						<i class="fas fa-check"> Good Comment</i>
+					</div>
+					<div class="mt-3 col-11 section ml-auto">
+						<p>Reply Comment (By another user/tutor/admin)</p>
+						<i class="fa fa-reply"> Reply</i>
+						<span> | </span>
+						<i class="fas fa-check"> Good Comment</i>					
+					</div>
+			        <div class="mt-3 col-12 section">
+						<!-- PHP Function would be good here, maybe -->
+						<p>User Comment</p>
+						<i class="fa fa-reply"> Reply</i>
+						<span> | </span>
+						<i class="fas fa-check"> Good Comment</i>
+					</div>
+				</div>
+			</div>
+		</article>
+	</div>
+
+	<footer class="container-fluid rounded-0">
+		<p>Footer Placeholder</p>
+	</footer>
+	
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
