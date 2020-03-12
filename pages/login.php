7d785a4faa094681a350ef5314b6d75d98cc06c9
Landen <Lmarchan@kent.edu>
2020-03-12 00:54:30 -0400

    Fixing login.php

--- pages/login.php
+++ pages/login.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Kent C++ Supplement</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="./css/index_CSS.css">
-	<link href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" rel="stylesheet"/> 
-	<link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Courier+Prime&display=swap" rel="stylesheet">
-
-</head>
-<body>
-	<?php
+<?php
 	include 'authLogin.php';
 
 	if (isset($_POST["username"]) && isset($_POST["password"])) {
@@ -34,15 +19,23 @@
 			break;
 		}
 	}
-	?>
-	<form action="login.php" method="post">
-		<label for="username">Username</label><br>
-		<!-- Keep username if just bad password -->
-		<input type="text" name="username" 
-		<?php if ($attempt == 0) echo "value=\"" . $_POST["username"] . "\""; ?> required><br>
-		<label for="password">Password</label><br>
-		<input type="password" name="password" required><br>
-		<input type="submit" value="Submit">
-	</form>
-	<?php if (isset($errorMsg)) echo $errorMsg; // Error message from attempt ?>
-</body>
+	require_once('../inc/header.inc.php');
+?>
+	<div class="content">
+		<div class="container">
+			<div class="row">
+				<form action="login.php" method="post">
+					<label for="username">Username</label><br>
+					<!-- Keep username if just bad password -->
+					<input type="text" name="username" 
+					<?php if ($attempt == 0) echo "value=\"" . $_POST["username"] . "\""; ?> required><br>
+					<label for="password">Password</label><br>
+					<input type="password" name="password" required><br>
+					<input type="submit" value="Submit">
+				</form>
+				<?php if (isset($errorMsg)) echo $errorMsg; // Error message from attempt ?>
+			</div>
+		</div>
+	</div>
+	
+<?php require_once('../inc/footer.inc.php'); ?>
