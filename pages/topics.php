6556f32c32a35dc3dc648df438329cbb38e7c1ec
Landen <Lmarchan@kent.edu>
2020-03-17 13:52:13 -0400

    Added topics.php

---
+++ pages/topics.php
@@ -0,0 +1,21 @@
+<?php require_once('../inc/header.inc.php'); ?>
+
+	<div class="content">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 mt-5">
+					<h1 class="text-center kentYellow">Course Topics</h1>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-12 mt-4 text-center links">
+					<p class="mt-3"><a href="#">Pointers</a></p>
+					<p><a href="#">Looping Mechanisms</a></p>
+					<p><a href="#">Arrays</a></p>
+					<p><a href="#">Lambda Expressions</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php require_once('../inc/footer.inc.php'); ?>
