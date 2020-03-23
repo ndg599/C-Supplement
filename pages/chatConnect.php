798364c645cf537076b17963a242d18f1de212fa
ndg599 <ndg599@gmail.com>
2020-03-23 00:37:54 -0400

    Chat connect page

--- pages/chatConnect.php
+++ pages/chatConnect.php
@@ -1,8 +1,27 @@
-<?php require_once('../inc/header.inc.php'); ?>
+<?php
+require_once('../pdoconfig.php');
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT ID,Username FROM Login WHERE Type='Tutor'";
+$result = mysqli_query($conn, $query);
+
+require_once('../inc/header.inc.php');
+?>
         <div class="content">
                 <div class="container">
-                        <div class="row">
-				<a href="https://www.kentcpp.com/pages/chat/chat.php">Chat Test</a>
+			<div class="row">
+			<div class="col-12">
+				<?php
+				while ($row = mysqli_fetch_assoc($result)) {
+					echo "<a href=\"https://www.kentcpp.com/pages/chat/chat.php?receiverid=" . $row["ID"]
+					. "\">" . $row["Username"] . "</a>";
+				}
+				?>
+			</div>
 			</div>
 		</div>
 	</div>
