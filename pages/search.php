7bc22523bd80c281665669abe8ccdd37a055ca3a
bjacob12 <56331778+bjacob12@users.noreply.github.com>
2020-04-06 20:37:16 -0400

    search.php

--- pages/search.php
+++ pages/search.php
@@ -5,7 +5,7 @@
 
 
 <?php
-
+/*
 error_reporting(-1);
 ini_set("display_errors","0");
 ini_set("log_errors",0);
@@ -53,7 +53,7 @@ mysqli_close($conn);
 echo "</div></div>";
 //require_once('../inc/footer.inc.php'); 
 
-
+*/
 ?>
 
 
@@ -66,5 +66,4 @@ echo "</div></div>";
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-</body>
-</html>
+
