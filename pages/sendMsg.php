d0e542a85f9f723359bc02ac52f93c2764ea5cc9
ndg599 <44713937+ndg599@users.noreply.github.com>
2020-04-07 17:16:22 -0400

    Message Text Fix

--- pages/sendMsg.php
+++ pages/sendMsg.php
@@ -9,12 +9,13 @@ if (!$conn) {
 // Insert message
 session_start();
 $stmt = mysqli_stmt_init($conn);
+$text = htmlspecialchars($_GET["text"]);
+$text = nl2br($_GET["text"]); // Convert new lines to HTML breaks
 mysqli_stmt_prepare($stmt, "INSERT INTO IM (SenderID, ReceiverID, IMText) VALUES (?,?,?)");
-mysqli_stmt_bind_param($stmt, "iis", $_SESSION["userid"], $_SESSION["partnerid"], $_GET["text"]);
+mysqli_stmt_bind_param($stmt, "iis", $_SESSION["userid"], $_SESSION["partnerid"], $text);
 mysqli_stmt_execute($stmt);
 sleep(0.1);
 // Find sent message
-//$stmt = mysqli_stmt_init($conn);
 mysqli_stmt_prepare($stmt, "SELECT MAX(IMNum) FROM IM WHERE SenderID=?");
 mysqli_stmt_bind_param($stmt, "i", $_SESSION["userid"]);
 mysqli_stmt_execute($stmt);
@@ -22,16 +23,14 @@ $result = mysqli_stmt_get_result($stmt);
 $row = mysqli_fetch_assoc($result);
 $lastMsgNum = $row["MAX(IMNum)"];
 // Return sent message
-//$stmt = mysqli_stmt_init($conn);
 mysqli_stmt_prepare($stmt, "SELECT IMText,Time FROM IM WHERE IMNum=?");
 mysqli_stmt_bind_param($stmt, "i", $lastMsgNum);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 $row = mysqli_fetch_assoc($result);
-$text = $_GET["text"];
 if ($text != $row["IMText"]) {
 	$text = "Failed to send message";
 }
 echo "<div class='sentMsgHead'>" . $_SESSION["username"] . " | " . $row["Time"] 
-	. "</div><div class='sentMsgText'>" . $text . "</div>";
+. "</div><div class='sentMsgText'>" . $text . "</div>";
 ?>
