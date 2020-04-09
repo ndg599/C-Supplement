887c10b3579e892d12d0d75661a27fc35d509d21
bjacob12 <56331778+bjacob12@users.noreply.github.com>
2020-04-09 04:48:48 -0400

    uploading reset files

---
+++ pages/reset.php
@@ -0,0 +1,140 @@
+
+
+
+<?php
+require_once('../inc/header.inc.php');
+function validatePassW($password) {
+	   $pattern = "/^.{8,30}$/";
+	   if ( preg_match($pattern, $password) ) {
+		 return true;
+	   }
+	   return false;
+	}
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once("dbconnect.php");
+if(isset($_GET["s"])){
+	echo "true";
+	$ss=$_GET["s"];
+	echo $ss;
+	$sql9="Select * from reset where hkey = '$ss'";
+	$ress=mysqli_query($conn,$sql9);
+	if (!$ress) {
+			printf("Error: %s\n", mysqli_error($conn));
+			exit();
+		}
+	$bool=0;
+	while($row=mysqli_fetch_array($ress)){
+		if($row["hkey"]==$ss){
+			$bool=1;
+			echo "matched found";	
+		}
+	}
+	if($bool==0){
+		header("Location: login.php?message=fail");
+	}
+
+}
+
+if(isset($_POST["npass"])&&isset($_POST["npassc"])){
+$new_pass=$_POST["npass"];
+$new_passc=$_POST["npassc"];
+
+if($new_pass==$new_passc){
+	if(validatePassW($new_pass)===false){
+		$error= $error . '<p class="red">Your Password is invalid. Must be 8 characters or more.</p>'; 
+	}else{
+	if(isset($_GET['s'])){
+		
+		$s=$_GET['s'];
+		$sql="Select * from reset where hkey = '$s'";
+		$result=mysqli_query($conn,$sql);
+		if (!$result) {
+			printf("Error: %s\n", mysqli_error($conn));
+			exit();
+		}
+		$row=mysqli_fetch_array($result);
+		$email=$row['Email'];
+		
+		$_hash=password_hash($new_pass,PASSWORD_BCRYPT);
+		//$sql2=$conn->prepare("INSERT INTO Login (Password) Values"."(?)"." where Email=$email");
+		$sql2=$conn->prepare("Update Login Set Password = ? where Email='$email'");
+		$sql2->bind_param("s",$_hash);
+		$result1=$sql2->execute();
+		if(false===$result1){
+			printf("error:%s\n", mysqli_error($conn));
+		}
+		if(! $result1 ) {
+			die('This account cannot be created. Please try again later.');
+		}
+
+		$sql3="Delete from reset where hkey='$s'";
+		$result3=mysqli_query($conn,$sql3);
+		if (!$result3) {
+			printf("Error: %s\n", mysqli_error($conn));
+			exit();
+		}
+		
+		$sql4="Update Login Set FailCount = '0' , Locked = '0' where Email = '$email' ";
+		$result4=mysqli_query($conn,$sql4);
+		if(false===$result4){
+			printf("error:%s\n", mysqli_error($conn));
+		}
+		if(! $result4 ) {
+			die("There's an error.");
+		}
+		
+		mysqli_close($conn);
+				
+		header("Location: login.php?message=changed");
+	}
+	}
+
+
+
+}else{echo "<p>Your passwords didn't match. Please try again</p>";}
+
+
+
+}
+
+
+
+
+?>
+
+
+<head>
+	<meta charset="UTF-8">
+	<title>Password Reset PHP</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<form class="login-form" action="" method="post">
+		<h2 class="form-title">New password</h2>
+		<!-- form validation messages -->
+		
+		<div class="form-group">
+			<label>New password&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</label>
+			<input type="password" name="npass" id="npass">
+		</div>
+		<div class="form-group">
+			<label>Confirm new password    </label>
+			<input type="password" name="npassc">
+		</div>
+		<div class="form-group">
+			<button type="submit" name="newpass" class="login-btn">Submit</button>
+		</div>
+	</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
---
+++ pages/resetpassword.php
@@ -0,0 +1,120 @@
+
+
+
+<?php
+require_once('../inc/header.inc.php'); 
+require_once("dbconnect.php");
+//removes notice reporting
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+if(isset($_POST['reset'])){
+	$_email=$_POST['email'];
+	
+	$sql =$conn->prepare("select * from Login where Email=?");
+		$sql->bind_param("s",$_email);
+		$retval=$sql->execute();
+	if (false===$retval) {
+		printf("Error1: %s\n", mysqli_error($conn));
+		exit();
+	}
+	if(! $retval ) {
+		die('Theres an error');
+	}
+	$row=$sql->get_result();
+	$em=$row->fetch_assoc();
+	if($em['Email']==null){
+	echo "This account doesn't exist. Please try again.";
+	}else {
+		$sel=bin2hex(random_bytes(8));
+		$token=random_bytes(32);
+		$url=sprintf('%rKentcpp.com/pages/reset.php?s=%s','ABS_URL',$sel,bin2hex($token));
+		$expires= new DateTime('NOW');
+		$expires->add(new DateInterval('PT01H'));
+		$eM=$em['Email'];
+		$sql4="Select * from reset";
+		$res=mysqli_query($conn,$sql4);
+		if(!$res){
+			printf("Error2: %s\n", mysqli_error($conn));
+				exit();
+		}		
+		$found="0";
+		while($row=mysqli_fetch_array($res)){
+			if($row['Email']===$eM){
+				$found=1;
+			}
+		}
+		
+		if($found===1){
+			
+			$sql1="Delete from reset where Email = '$eM'";
+			$results5= mysqli_query($conn,$sql1);
+			if(!$results5){
+				printf("Error2: %s\n", mysqli_error($conn));
+				exit();
+			}
+		}
+		$hashed=hash('sha256',$token);
+		
+		$sql2 =$conn->prepare("Insert into reset". "(Email,hkey,token)"."Values"."(?,?,?)");
+		$sql2->bind_param("sss",$eM,$sel,$hashed);
+		$results4= $sql2->execute();
+		if(false===$results4){
+			printf("Error4: %s\n", mysqli_error($conn));
+			exit();
+		}
+		if(!$results4){die('Your request cannot be sent at this time. Please try again later.');}
+
+
+		$send=$em['Email'];
+		$subj='Password reset link';
+		$mess='<p>Below is a link to reset your password</p>';
+		$mess.=sprintf('<a href="%href="%s">%s</a></p>',"",$url,$url);
+		$head="From: donotreply@kentcpp.com\r\n";
+		$head.="Reply-To: donotreply.KentCpp.com\r\n";
+		$head.= "Content-Type: text/html\r\n";
+		$sent = mail($send,$subj,$mess,$head);
+		header("Location: login.php?message=sent");
+	}
+	
+	
+
+}
+
+/*
+Add command later.
+
+
+CREATE EVENT hourlyreset
+    ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 HOUR
+  DO
+  DELETE from reset WHERE
+    expires < DATEADD(day, -1, GETDATE())
+
+
+ #1105 Event scheduler is switched off, use SET GLOBAL event_scheduler=ON
+
+
+*/
+
+
+
+
+?>
+<br><br><br>
+<div class="container d-flex h-100">
+	<div class="row">
+	<div class="col-12">
+	<h1>Reset Password</h1>
+<form method="post">
+	<table width = "500" border = "0" cellspacing = "1" cellpadding = "1">
+	<tr>
+	<td >Input your email</td>
+	<td>
+	<input name = "email" type = "text" id = "email">
+	<td><input name="reset" type = "submit" id="reset" value=" reset password"></td></td></tr>
+</div></div></div>
+
+
+
+
+
+<?php //require_once('../inc/footer.inc.php'); ?>
