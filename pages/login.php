1f44bfa8e53ee8d6ec144228159a0dfca94ce394
bjacob12 <56331778+bjacob12@users.noreply.github.com>
2020-04-09 18:21:56 -0400

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -44,10 +44,10 @@
 			
 			$user=$_POST["username"];
 			require("dbconnect.php");
-		
+		/*
 			$sql1=$conn->prepare("SELECT * FROM Login WHERE Username = ?");
 				
-			/*$sql1->bind_param("s",$user);
+			$sql1->bind_param("s",$user);
 			$sql1->execute();
 			$result2=$sql1->get_result();
 		
