2ffad734787fb131013c8b502d08b8d3ddb50421
bjacob12 <56331778+bjacob12@users.noreply.github.com>
2020-04-09 19:11:25 -0400

    upload dbconnect2

---
+++ pages/dbconnect2.php
@@ -0,0 +1,13 @@
+<?php
+	/* Used to open database */
+	require_once('../pdoconfig.php');
+	
+	
+	// Create connection
+	$conn = new mysqli(localhost, u664461894_group2 group2kentcpp, u664461894_cpp;
+
+	if(!$conn){
+		die('Could not connect: ' . mysqli_error($conn));
+	}
+	//else
+	//  echo "Database Connection was successful";
