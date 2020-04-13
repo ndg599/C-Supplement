cf93cf796bf78db6ab046a0b9423fcc324b63337
ndg599 <ndg599@gmail.com>
2020-04-13 18:18:16 -0400

    Working makePrgQuiz

--- pages/makePrgQuiz.js
+++ pages/makePrgQuiz.js
@@ -1,9 +1,10 @@
+/*
 function ProgQuiz(desc, ans)
 {
 	this.desc = desc;
 	this.ans = ans;
 }
-
+*/
 var answerIndex = 1;
 var answerDiv = document.getElementById("answers");
 
@@ -13,8 +14,8 @@ function init()
 {
 	var addOutput = document.getElementById("addAnswer");
 	addOutput.onclick = addAnswer;
-	var sendButton = document.getElementById("submit");
-	sendButton.onclick = sendQuiz;
+	//var sendButton = document.getElementById("submit");
+	//sendButton.onclick = sendQuiz;
 }
 
 function addAnswer()
@@ -23,13 +24,13 @@ function addAnswer()
 	var p = document.createElement("p");
 	p.innerHTML = "Acceptable Output " + answerIndex;
 	var ta = document.createElement("textarea");
-	ta.id = "answer" + answerIndex;
+	ta.name = "answer" + answerIndex;
 	ta.cols = 50;
 	ta.rows = 10;
 	answerDiv.appendChild(p);
 	answerDiv.appendChild(ta);
 }
-
+/*
 function sendQuiz()
 {
 	var answers = new Array();
@@ -40,7 +41,7 @@ function sendQuiz()
 	var desc = document.getElementById("desc").value;
 	var newQuiz = new ProgQuiz(desc, answers);
 	console.log(newQuiz);
-/*
+
 	// Send AJAX request to compile program, print result
 	var text = encodeURIComponent(document.getElementById("textBox").value);
 	var request = new XMLHttpRequest();
@@ -49,8 +50,9 @@ function sendQuiz()
 			result.innerHTML = this.responseText;
 		}
 	};
-	request.open("GET", "programQuiz/sendProgram.php?text=" + text);
-	request.setRequestHeader("Content-Type", "text/x-c;chatset=UTF-8");
+	request.open("GET", "sendPrgQuiz.php?desc=" + desc);
+	request.setRequestHeader("Content-Type", "text/plain;chatset=UTF-8");
 	request.send();
-*/
+
 }
+*/
--- pages/makePrgQuiz.php
+++ pages/makePrgQuiz.php
@@ -1,17 +1,52 @@
-<?php require_once('../inc/header.inc.php'); ?>
+<?php
+
+if (isset($_POST["desc"])) {
+    require_once('../pdoconfig.php');
+
+	$desc = htmlspecialchars($_POST["desc"]);
+
+	$i = 1;
+	$answers = Array();
+	while (isset($_POST["answer" . $i])) {
+		$answers[$i] = htmlspecialchars($_POST["answer" . $i]);
+		$i++;
+	}
+
+	$answers = json_encode($answers);
+
+    // Setup link to database
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+	$stmt = mysqli_stmt_init($conn);
+	mysqli_stmt_prepare($stmt, "INSERT INTO ProgQuiz VALUES (NULL,?,?)");
+	mysqli_stmt_bind_param($stmt, "ss", $desc, $answers);
+	$result = mysqli_stmt_execute($stmt);
+
+	if (!$result) {
+		die("Query failed: " .  mysqli_error($conn));
+	}
+
+}
+ 
+require_once('../inc/header.inc.php');
+?>
 <div class="content">
  <div class="container">
   <div class="row">
    <div class="col-12">
 	<p>Quiz Description:</p>
-	<textarea id="desc" cols="70" rows="5"></textarea>
-	<div id="answers">
-		<p>Acceptable Output 1:</p>
-		<textarea id="answer1" cols="50" rows="10"></textarea><br>
-	</div>
-	<button id="addAnswer">Add Acceptable Output</button><br>
-	<button id="submit">Submit Quiz</button>
-	<p id="result"></p>
+	<form action="makePrgQuiz.php" method="post">
+		<textarea name="desc" cols="70" rows="5"></textarea>
+		<div id="answers">
+			<p>Acceptable Output 1:</p>
+			<textarea name="answer1" cols="50" rows="10"></textarea><br>
+		</div>
+		<button type="button" id="addAnswer">Add Acceptable Output</button><br>
+		<br><input type="submit" id="submit" value="Submit Quiz">
+	</form>
    </div>
   </div>
  </div>
