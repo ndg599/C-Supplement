b6f4dfcfecb3caad8f3c42cd68fd5eafed79fd0c
ndg599 <ndg599@gmail.com>
2020-04-14 13:10:06 -0400

    Made addCompileFeedback.php

---
+++ pages/addCompileFeedback.php
@@ -0,0 +1,45 @@
+<?php
+
+if (isset($_POST["target"])) {
+    require_once('../pdoconfig.php');
+
+	$target = htmlspecialchars($_POST["target"]);
+	$feedback = htmlspecialchars($_POST["feedback"]);
+
+    // Setup link to database
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+	$stmt = mysqli_stmt_init($conn);
+	mysqli_stmt_prepare($stmt, "INSERT INTO CompileFeedback VALUES (NULL,?,?)");
+	mysqli_stmt_bind_param($stmt, "ss", $target, $feedback);
+	$result = mysqli_stmt_execute($stmt);
+
+	if (!$result) {
+		die("Query failed: " .  mysqli_error($conn));
+	}
+	
+	$submitMsg = "Feedback message submitted.";
+}
+ 
+require_once('../inc/header.inc.php');
+?>
+<div class="content">
+ <div class="container">
+  <div class="row">
+   <div class="col-12">
+	<?php if (isset($submitMsg)) { echo "<p>$submitMsg</p>"; } ?>
+	<form action="addCompileFeedback.php" method="post">
+		<p>Enter the string to identify the compile error:</p>
+		<textarea name="target" cols="70" rows="5"></textarea>
+		<p>Enter the feedback message to be received by the student:</p>
+		<textarea name="feedback" cols="70" rows="5"></textarea>
+		<br><input type="submit" value="Submit">
+	</form>
+   </div>
+  </div>
+ </div>
+</div>
+<?php require_once('../inc/footer.inc.php');?>
