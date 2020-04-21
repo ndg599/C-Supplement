66522f50b72ec29f8adbe5005329af99e5d41f00
ndg599 <ndg599@gmail.com>
2020-04-21 17:03:16 -0400

    program quiz ID error

--- pages/programQuiz.php
+++ pages/programQuiz.php
@@ -1,20 +1,22 @@
 <?php 
 require_once("../pdoconfig.php");
 
-// Setup link to database
-$conn = mysqli_connect($servername, $username, $password, $database);
-if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-}
+if ($_GET["ID"]) {
+	// Setup link to database
+	$conn = mysqli_connect($servername, $username, $password, $database);
+	if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+	}
 
-$stmt = mysqli_stmt_init($conn);
-mysqli_stmt_prepare($stmt, "SELECT Descr FROM ProgQuiz WHERE ID=?");
-mysqli_stmt_bind_param($stmt, "i", $_GET["ID"]);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
+	$stmt = mysqli_stmt_init($conn);
+	mysqli_stmt_prepare($stmt, "SELECT Descr FROM ProgQuiz WHERE ID=?");
+	mysqli_stmt_bind_param($stmt, "i", $_GET["ID"]);
+	mysqli_stmt_execute($stmt);
+	$result = mysqli_stmt_get_result($stmt);
 
-if (!$result) {
-	die("Query failed: " .  mysqli_error($conn));
+	if (!$result) {
+		die("Query failed: " .  mysqli_error($conn));
+	}
 }
 
 $row = mysqli_fetch_assoc($result);
@@ -25,7 +27,7 @@ require_once('../inc/header.inc.php');
  <div class="container">
   <div class="row">
 	<div class="col-12">
-		<br><p><?php echo $row["Descr"]; ?></p>
+		<br><p><?php if ($row["Descr"]) { echo $row["Descr"]; } ?></p>
 		<div id="textBox" style="background-color: Black" class="lang-clike"><code></code></div>
 		<input type="button" id="send" value="Send"><br><br>
 		<p id="result"></p>
