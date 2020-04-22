33162a12707196688dd066446a959d55110834a2
ndg599 <ndg599@gmail.com>
2020-04-22 17:46:29 -0400

    article image input

--- pages/makeArticle.php
+++ pages/makeArticle.php
@@ -1,9 +1,39 @@
 <?php
-if (isset($_POST["title"])) {
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_POST["articleTitle"])) {
+	class Image
+	{
+		public int $position;
+		public string $filename;
+		public string $caption;
+		public string $alt;
+	}
+
+	function getImageJSON($sect)
+	{
+		$img = array();
+		$i = 1;
+		echo $sect . "ImgPos" . $i . "\n";
+		while (isset($_POST[$sect . "ImgPos" . $i])) {
+			$img[$i-1] = new Image();
+			$img[$i-1]->position = $_POST[$sect . "ImgPos" . $i];
+			$img[$i-1]->filename = $_POST[$sect . "ImgFile" . $i];
+			$img[$i-1]->caption = $_POST[$sect . "ImgCap" . $i];
+			$img[$i-1]->alt = $_POST[$sect . "ImgAlt" . $i];
+			$i++;
+			echo $sect . "ImgPos" . $i . "\n";
+		}
+		return JSON_encode($img);
+	}
+
     require_once('../pdoconfig.php');
 
-	$title = nl2br(htmlspecialchars($_POST["title"]));
-	$body = nl2br(htmlspecialchars($_POST["body"]));
+	$title = nl2br(htmlspecialchars($_POST["articleTitle"]));
+	$body = nl2br(htmlspecialchars($_POST["articleBody"]));
+	$imgJSON = getImageJSON("article");
 
     // Setup link to database
     $conn = mysqli_connect($servername, $username, $password, $database);
@@ -11,6 +41,9 @@ if (isset($_POST["title"])) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
+	echo "INSERT INTO Article VALUES (NULL,$title,$body,NULL,NULL,$imgJSON)";
+	return;
+/*
 	$stmt = mysqli_stmt_init($conn);
 	mysqli_stmt_prepare($stmt, "INSERT INTO Article VALUES (NULL,?,?,NULL,NULL)");
 	mysqli_stmt_bind_param($stmt, "ss", $title, $body);
@@ -19,7 +52,7 @@ if (isset($_POST["title"])) {
 	if (!$result) {
 		die("Query failed: " .  mysqli_error($conn));
 	}
-	
+*/	
 	$query = "SELECT Max(TopicID) FROM Article";
 	$result = mysqli_query($conn, $query);
 
