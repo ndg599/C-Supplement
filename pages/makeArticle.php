f801f31e5c7c4671c3fb2f85477d251f44d5948a
ndg599 <ndg599@gmail.com>
2020-04-23 05:03:33 -0400

    Changed article image adding method

--- pages/makeArticle.php
+++ pages/makeArticle.php
@@ -6,33 +6,51 @@ error_reporting(E_ALL);
 if (isset($_POST["articleTitle"])) {
 	class Image
 	{
-		public int $position;
+		public int $line;
 		public string $filename;
 		public string $caption;
 		public string $alt;
 	}
 
-	function getImageJSON($sect)
+	function getImageJSON($sect, $body)
 	{
 		$img = array();
-		$i = 1;
-		while (isset($_POST[$sect . "ImgPos" . $i])) {
-			echo $sect . "ImgPos" . $i . "\n";
-			$img[$i-1] = new Image();
-			$img[$i-1]->position = $_POST[$sect . "ImgPos" . $i];
-			$img[$i-1]->filename = $_POST[$sect . "ImgFile" . $i];
-			$img[$i-1]->caption = $_POST[$sect . "ImgCap" . $i];
-			$img[$i-1]->alt = $_POST[$sect . "ImgAlt" . $i];
+		$i = 0;
+		$lastPos = 0;
+		$lineCount = 0;
+		while ($pos = strpos($body, "[img", $lastPos)) { // Search for position of each image marker
+			$id = $body[$pos+4]; // Get id of image (NOTE: will only get id that's under 10... fix this)
+			$lineCount += substr_count($body, "\n", $lastPos, $pos-$lastPos); // Find line the image is on
+			$img[$i] = new Image();
+			$img[$i]->line = $lineCount;
+			$img[$i]->filename = $_POST[$sect . "ImgFile" . $id];
+			$img[$i]->caption = $_POST[$sect . "ImgCap" . $id];
+			$img[$i]->alt = $_POST[$sect . "ImgAlt" . $id];
 			$i++;
+			$lastPos = $pos+1;
 		}
 		return JSON_encode($img);
 	}
 
+	function removeImageMarkers($sect, $body)
+	{
+		$i = 1;
+		while (isset($_POST[$sect . "ImgFile$i"])) {
+			$body = str_replace("[img$i]", "", $body);
+			$i++;
+		}
+		return $body;
+	}
+
     require_once('../pdoconfig.php');
 
-	$title = nl2br(htmlspecialchars($_POST["articleTitle"]));
-	$body = nl2br(htmlspecialchars($_POST["articleBody"]));
-	$imgJSON = getImageJSON("article");
+	$title = htmlspecialchars($_POST["articleTitle"]);
+	$body = $_POST["articleBody"];
+	$imgJSON = getImageJSON("article", $body);
+	$body = nl2br(htmlspecialchars(removeImageMarkers("article", $body)));
+	echo "$body<br>";
+	echo "$imgJSON<br>";
+	return;
 
     // Setup link to database
     $conn = mysqli_connect($servername, $username, $password, $database);
@@ -63,8 +81,8 @@ if (isset($_POST["articleTitle"])) {
 	while (isset($_POST["sub$i"."Body"])) {
 		$title = htmlspecialchars($_POST["sub$i"."Title"]);
 		$body = htmlspecialchars($_POST["sub$i"."Body"]);
-		$code = "";
-		$imgJSON = getImageJSON("sub$i");
+		$imgJSON = getImageJSON("sub$i", $body);
+		$body = removeImageCodes($body);
 
 		//echo "INSERT INTO Subtopics VALUES ($articleID,$i,$title,$code,$body,$imgJSON)\n";
 		$stmt = mysqli_stmt_init($conn);
@@ -101,11 +119,13 @@ require_once('../inc/header.inc.php');
 				<textarea name="articleBody" cols="70" rows="5"></textarea><br>
 			</div>
 		</div>
-		<br><button type="button" id="addSub">Add SubSection</button>
-		<button type="button" id="addImg">Add Image</button>
-		<button type="button" id="addCode">Add Code Snippet</button>
-		<button type="button" id="addVid">Add Video</button><br><br>
-		<input type="submit" id="submit" value="Submit Article">
+		<div id="taskbar">
+			<br><button type="button" id="addSub">Add SubSection</button>
+			<button type="button" id="addImg">Add Image</button>
+			<button type="button" id="addCode">Add Code Snippet</button>
+			<button type="button" id="addVid">Add Video</button><br><br>
+			<input type="submit" id="submit" value="Submit Article">
+		</div>
 	</form>
    </div>
   </div>
