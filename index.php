16f8b60b7e3fbd77e15492543e2d96b9d4da9978
Tutlegoss <tutlegoss@aol.com>
2020-04-26 12:51:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 						</tr>						
 					</table>
 					<div class="text-center">
-						<a href="#" class="btn btnKent">CS I Index</a>
+						<a href="./pages/topics.php?Class=CSI" class="btn btnKent">CS I Index</a>
 					</div>
 				</div>
 				
@@ -67,7 +67,7 @@
 						</tr>						
 					</table>
 					<div class="text-center">
-						<a href="#" class="btn btnKent">CS II Index</a>
+						<a href="./pages/topics.php?Class=CSII" class="btn btnKent">CS II Index</a>
 					</div>
 				</div>
 
@@ -99,38 +99,36 @@
 						</tr>						
 					</table>
 					<div class="text-center">
-						<a href="#" class="btn btnKent">CS III Index</a>
+						<a href="./pages/topics.php?Class=CSIII" class="btn btnKent">CS III Index</a>
 					</div>
 				</div>
 				
-				<?php if (isset($_SESSION["loggedin"])): ?>
-					<div class="col-cust-img mb-5"> 
-						<img src="./img/IM.png" class="img-view mx-auto d-block" alt="Chat Word Art">
+				<div class="col-cust-img mb-5"> 
+					<img src="./img/IM.png" class="img-view mx-auto d-block" alt="Chat Word Art">
+				</div>
+				<div class="col-cust-info mb-5">
+					<h5 class="kentYellow text-center">Chat with a Tutor</h5>				
+					<table class="table text-white">
+						<tr>
+							<td class="kentBlue">Landen:</td>
+							<td>Monday/Wednesday</td>
+							<td>08:00a - 12:00p</td>
+						</tr>
+						<tr>
+							<td class="kentBlue">Trey:</td>
+							<td>Tuesday/Thursday</td>
+							<td>09:00a - 01:00p</td>
+						</tr>
+						<tr>
+							<td class="kentBlue">Tutor3:</td>
+							<td>Friday</td>
+							<td>01:00p - 05:00p</td>
+						</tr>				
+					</table>				
+					<div class="text-center">
+						<a href="./pages/chatConnect.php" class="btn btnKent">IM Client</a>
 					</div>
-					<div class="col-cust-info mb-5">
-						<h5 class="kentYellow text-center">Chat with a Tutor</h5>				
-						<table class="table text-white">
-							<tr>
-								<td class="kentBlue">Landen:</td>
-								<td>Monday/Wednesday</td>
-								<td>08:00a - 12:00p</td>
-							</tr>
-							<tr>
-								<td class="kentBlue">Trey:</td>
-								<td>Tuesday/Thursday</td>
-								<td>09:00a - 01:00p</td>
-							</tr>
-							<tr>
-								<td class="kentBlue">Tutor3:</td>
-								<td>Friday</td>
-								<td>01:00p - 05:00p</td>
-							</tr>				
-						</table>				
-						<div class="text-center">
-							<a href="#" class="btn btnKent">IM Client</a>
-						</div>
-					</div>
-				<?php endif; ?>
+				</div>
 			</div>
 		</div>
 	</div>
