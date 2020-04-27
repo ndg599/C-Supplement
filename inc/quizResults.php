22c11f5df671739434e3ac3b2a66a48e80586efc
Tutlegoss <tutlegoss@aol.com>
2020-04-27 05:24:27 -0400

    Update quizResults.php

--- inc/quizResults.php
+++ inc/quizResults.php
@@ -3,29 +3,22 @@
 
 	function dispAns()
 	{
+		print_r($_POST);
+		$incoming = json_decode($_POST["choices"], false);
 		global $conn;
 		try {
-			/*
-			else {
-				if(!($sql_images = $conn->prepare("SELECT Title, Path, travelimagedetails.ImageID as ImageID
-				                                   FROM   (geocountries JOIN travelimagedetails) JOIN travelimage
-												   WHERE  Continent = ? AND ISO = ?
-													 AND ISO = CountryCodeISO AND travelimagedetails.ImageID = travelimage.ImageID"))) {
-					echo "Prepare failed: (" . $conn->errno . ")" . $conn->error;
-					return;
-				}
-				$sql_images->bind_param("ss",  $_GET['continent'], $_GET['country'],);
-			}
+			if(!($sql_results = $conn->prepare("SELECT Ans, Exp FROM Questions SORT BY QNum ASC")))
+				return;
 
-			$sql_images->execute();
-			$sql_image = $sql_images->get_result();
+			$sql_results->execute();
+			$sql_results = $sql_results->get_result();
 			$rowArray = array();
-			while ($row = $sql_image->fetch_assoc())
-				$rowArray[] = $row; */
-			$rowArray = array("One", "two", "Three");
+			while ($row = $sql_results->fetch_assoc())
+				$rowArray[] = $row; 
+
 			echo json_encode($rowArray);
 		} catch (Exception $e) {
-			die("ImageFilter.php: " . $e);
+			die("quizResults.php: " . $e);
 		}
 	}		
 	
