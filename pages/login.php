ad35a1171df33cfad082f8c954d48aeaa788dbd9
Tutlegoss <tutlegoss@aol.com>
2020-04-28 16:35:45 -0400

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -132,8 +132,8 @@
 					<label class="kentBlue mt-2" for="password">Password</label>
 					<input type="password" name="password" required><br>
 					<input class="btn btnKent mt-2" id="btnMv" type="submit" value="Sign In">
-					<a href="./signup.php" class="btn btnKent" id="suMv">Sign Up</a>
-					<br><a href="resetpassword.php">Forgot Password?</a>
+					<br><a href="resetpassword.php" class="kentBlue">Forgot Password?</a>
+					<br><a href="./signup.php" class="kentYellow">Sign Up</a>
 				</form>
 				
 				<?php if (isset($errorMsg)) echo $errorMsg; // Error message from attempt ?>
