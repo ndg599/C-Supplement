ee1d4c3edc76b86859eb463b103a60e0a10abd60
Tutlegoss <tutlegoss@aol.com>
2020-04-29 08:37:53 -0400

    Update article2.php

--- pages/article2.php
+++ pages/article2.php
@@ -497,9 +497,17 @@ if(isset($_POST['reply'])){
 			<div class="container">
 				<div class="row justify-content-center">
 					<div class="col-12 col-lg-9">
-						<?php   if($videoLink != NULL) {
-									echo '<div class="embed-responsive embed-responsive-16by9">
-										      <iframe class="embed-responsive-item" src="'. $videoLink . '" allowfullscreen>
+						<?php   if($_GET['ID'] == 11) /*if($videoLink != NULL)*/ {
+									echo '<div class="embed-responsive embed-responsive-16by9 mb-4">
+										      <iframe class="embed-responsive-item" src="https://youtu.be/4bh6ianNFtM" allowfullscreen>
+									          </iframe>
+										  </div>';
+									echo '<div class="embed-responsive embed-responsive-16by9 mb-4">
+										      <iframe class="embed-responsive-item" src="https://youtu.be/en57ADDlSms" allowfullscreen>
+									          </iframe>
+										  </div>';
+									echo '<div class="embed-responsive embed-responsive-16by9 mb-4">
+										      <iframe class="embed-responsive-item" src="https://youtu.be/aO1y4ROInb0" allowfullscreen>
 									          </iframe>
 										  </div>';
 						        } else
