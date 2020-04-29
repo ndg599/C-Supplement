31dabbd3d5cac718d4ec2789a0e1247fe5a58453
Tutlegoss <tutlegoss@aol.com>
2020-04-29 04:13:24 -0400

    Update makeArticle.js

--- pages/makeArticle.js
+++ pages/makeArticle.js
@@ -109,7 +109,7 @@ function addImg()
 		div.id = sect.id + "img" + index;
 
 		var p = document.createElement("p");
-		element.classList.add("magenta");
+		p.classList.add("magenta");
 		p.innerHTML = "<br>Image " + index;
 		div.appendChild(p);
 
@@ -134,7 +134,7 @@ function addCode()
 		div.id = sect.id + "code" + index;
 
 		var p = document.createElement("p");
-		element.classList.add("green");
+		p.classList.add("green");
 		p.innerHTML = "<br>Code Snippet " + index;
 		div.appendChild(p);
 
