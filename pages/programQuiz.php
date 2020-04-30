d802b25a5c1289cafe586405847efc0ff2f4e37f
ndg599 <ndg599@gmail.com>
2020-04-30 04:54:39 -0400

    Added tips to program quiz

--- pages/programQuiz.php
+++ pages/programQuiz.php
@@ -12,7 +12,7 @@ if ($_GET["ID"]) {
 	require_once("dbconnect2.php");
 
 	$stmt = mysqli_stmt_init($conn);
-	mysqli_stmt_prepare($stmt, "SELECT Descr FROM ProgQuiz WHERE ID=?");
+	mysqli_stmt_prepare($stmt, "SELECT Descr,Tips FROM ProgQuiz WHERE ID=?");
 	mysqli_stmt_bind_param($stmt, "i", $_GET["ID"]);
 	mysqli_stmt_execute($stmt);
 	$result = mysqli_stmt_get_result($stmt);
@@ -26,19 +26,26 @@ if ($_GET["ID"]) {
 else {
 	require_once("dbconnect2.php");
 	$query = "SELECT ID, Descr FROM ProgQuiz";
-	$result = mysqli_query($query, $conn);
+	$result = mysqli_query($conn, $query);
+
+	if (!$result) {
+		die("Query failed: " .  mysqli_error($conn));
+	}
 
 	require_once('../inc/header.inc.php');
-	echo
-'	<div class="content">
+	echo '
+	<div class="content">
 	 <div class="container">
 	  <div class="row">
-		<div class="col-12">';
+		<div class="col-12">
+			<p><br>Select a programming quiz:</p>';
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo '<a href="programQuiz.php?ID=' , $row['ID'] . '">' . $row['Descr'] . '</a><br>';
+				$rowid = $row['ID'];
+				$desc = $row['Descr'];
+				echo "<p>$rowid - <a href='programQuiz.php?ID=$rowid'>$desc</a></p>";
 			}
-	echo
-'		</div>
+	echo '
+		</div>
 	  </div>
 	 </div>
 	</div>';
@@ -56,6 +63,16 @@ require_once('../inc/header.inc.php');
 		<div id="textBox" style="background-color: Black" class="lang-clike"><code></code></div>
 		<input type="button" id="send" value="Send"><br><br>
 		<p id="result"></p>
+		<button type="button" id="showTip">Request a Tip</button><br><br>
+		<?php
+		$tips = json_decode($row["Tips"], true);
+		$i = 1;
+		while ($tips[$i]) {
+			$tip = $tips[$i];
+			echo "<p id='tip$i' hidden>Tip $i: $tip</p>";
+			$i++;
+		}
+		?>
 	</div>
   </div>
  </div>
